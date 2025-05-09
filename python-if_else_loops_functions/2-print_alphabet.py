1d9418306f1e337f59899584d9476f12caef7dc3
fadumaaah <10507@holbertonstudents.com>
2025-05-09 14:55:10 +1000

    try for loop

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-ascii_value = 97
-while ascii_value < 122:
-    print(f"{chr(ascii_value)}", end="")
-    ascii_value = ascii_value + 1
+for i in range(97, 122):
+    print(f"{chr(i)}", end="")
