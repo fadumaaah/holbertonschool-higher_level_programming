dc36552ee66fbb6e18196daf8136b6b757741e52
fadumaaah <10507@holbertonstudents.com>
2025-05-13 07:57:52 +1000

    add condition for empty matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            if i < len(row) - 1:
-                # to account for extra space printed at end of row
-                print("{:d}".format(row[i]), end=" ")
-            else:
-                print("{:d}".format(row[i]))
+        if row:
+            for i in range(len(row)):
+                if i < len(row) - 1:
+                    # to account for extra space printed at end of row
+                    print("{:d}".format(row[i]), end=" ")
+                else:
+                    print("{:d}".format(row[i]))
+        else:
+            print()
