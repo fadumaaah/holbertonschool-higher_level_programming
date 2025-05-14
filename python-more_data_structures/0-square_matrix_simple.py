a91926dbbd8f3b25eb82b6828bbc125ef1d4b1fb
fvdumaa <fadumaaah@gmail.com>
2025-05-14 17:38:36 +1000

    0-square-matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+def square_matrix_simple(matrix=[]):
+    squared_matrix = [row[:] for row in matrix]
+    for row in squared_matrix:
+        for i in range(len(row)):
+            row[i] = row[i] ** 2
+
+    return squared_matrix
