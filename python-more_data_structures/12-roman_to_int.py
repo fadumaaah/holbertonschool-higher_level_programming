001c0c999f8a8bc709659d16cf25df059bd415b8
fvdumaa <fadumaaah@gmail.com>
2025-05-17 17:48:45 +1000

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,33 @@
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    else:
-        if roman_string <=9
+
+    total = 0
+    previous = 0
+
+    for char in reversed(roman_string):
+        if char == 'M':
+            value = 1000
+        elif char == 'D':
+            value = 500
+        elif char == 'C':
+            value = 100
+        elif char == 'L':
+            value = 50
+        elif char == 'X':
+            value = 10
+        elif char == 'V':
+            value = 5
+        elif char == 'I':
+            value = 1
+        else:
+            return 0
+
+        if value >= previous:
+            total += value
+        else:
+            total -= value
+
+        previous = value
+
+    return total
