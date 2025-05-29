9425ae52ef83e3f1cd5d53f27a27e79673e2c112
fvdumaa <fadumaaah@gmail.com>
2025-05-29 17:53:09 +1000

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+argv = sys.argv[1:]
+if len(argv) == 0:
+    print("0 arguments.")
+elif len(argv) == 1:
+    print("1 argument:")
+    for i in range(len(argv)):
+        print("{}: {}".format(i, argv[i]))
+else:
+    print("{} arguments".format(len(argv)))
+    for i in range(len(argv)):
+        print("{}: {}".format(i, argv[i]))
