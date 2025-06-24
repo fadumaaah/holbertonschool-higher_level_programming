711aef5919c6175bfd27a081dea0b565d93fc249
fadumaaah <10507@holbertonstudents.com>
2025-06-24 12:54:48 +1000

    add docstrings

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+This module defines the Square class
+"""
 class Square:
+    """
+    Represents a square.
+    
+    Attributes:
+        None
+    
+    Methods:
+        None 
+    """
     pass
