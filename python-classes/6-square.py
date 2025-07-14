14434345ed8a1bc95d3a88fbf4f8b45675607b84
fadumaaah <10507@holbertonstudents.com>
2025-07-14 13:31:28 +1000

    6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class
+"""
+
+
+class Square:
+    """
+    Represents a square with a private size attribute.
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integer")
+        if not all(isinstance(num, int) and num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integer")
+        self.__position = value
+
+    def area(self):
+        """Returns area of square"""
+        return self.size**2
+
+    def my_print(self):
+        """Prints a square with #"""
+        if self.size == 0:
+            print()
+            return
+
+        for _ in range(self.position[1]):
+            print()
+
+        for _ in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
